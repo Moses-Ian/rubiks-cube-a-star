a84bce72f917ee6d0c9cb19128700e778f53b211
Ian Moses <imoses2@hotmail.com>
2023-02-03 20:57:42 -0700

    initial commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+	let canvas = createCanvas(300, 300);
+	canvas.parent('sketch-container');
+}
+
+function draw() {
+  // put drawing code here
+	background(0);
+}
