dcb919633a2968656a23b1223209f9712f3536cc
Ian Moses <imoses2@hotmail.com>
2023-02-04 23:09:10 -0700

    WE HAVE SPIN

--- box.js
+++ box.js
@@ -11,6 +11,9 @@ const PI = Math.PI;
 const HALF_PI = Math.PI / 2;
 
 const ZERO2D = new THREE.Vector2(0, 0);
+const X3D = new THREE.Vector3(1, 0, 0);
+const Y3D = new THREE.Vector3(0, -1, 0);
+const Z3D = new THREE.Vector3(0, 0, 1);
 	
 class Box {
 	
@@ -20,20 +23,19 @@ class Box {
 		this.oldIndex = this.index;
 		this.len = len;
 		this.rotation = new THREE.Vector3(0, 0, 0);
-		this.normal = new THREE.Vector3(-1, 0, 0);
-		this.start = new THREE.Vector3(1, 0, 0);
 		
 		this.planes = new Array(6);
 		this.outlines = new Array(6);
 	
 		// make the planes
+		let mat = new THREE.LineBasicMaterial( { color: 0x000000 } );
+
 		let geometry = new THREE.PlaneGeometry( this.len, this.len );
 		geometry.rotateY(HALF_PI);
 		geometry.translate(this.len/2, 0, 0);
 		let material = new THREE.MeshBasicMaterial( {color: colors[0], side: THREE.DoubleSide} );
 		this.planes[0] = new THREE.Mesh( geometry, material );
 		let geo = new THREE.EdgesGeometry( geometry );
-		let mat = new THREE.LineBasicMaterial( { color: 0x000000 } );
 		this.outlines[0] = new THREE.LineSegments( geo, mat );
 		
 		geometry = new THREE.PlaneGeometry( this.len, this.len );
@@ -87,7 +89,6 @@ class Box {
 			let temp = new THREE.Vector2(this.pos.y, this.pos.z)
 				.rotateAround(ZERO2D, turnSpeed);
 			this.pos = new THREE.Vector3(this.pos.x, temp.x, temp.y);	// this is confusing -> remember that temp is 2D and pos is 3D
-			// this.group.position.set(this.pos);
 			this.group.position.x = this.pos.x;
 			this.group.position.y = this.pos.y;
 			this.group.position.z = this.pos.z;
@@ -96,19 +97,7 @@ class Box {
 				.rotateAround(ZERO2D, turnSpeed);
 			this.index = new THREE.Vector3(this.index.x, temp.x, temp.y);
 			
-			// this.rotation = this.turn(new THREE.Vector3(1, 0, 0));
-			
-			// this.normal = new THREE.Vector3(
-				// this.normal.x,
-				// this.normal.y * Math.cos(turnSpeed) - this.normal.z * Math.sin(turnSpeed),
-				// this.normal.y * Math.sin(turnSpeed) + this.normal.z * Math.cos(turnSpeed)
-			// );
-			
-			// this.rotation = new THREE.Vector3(
-				// this.rotation.x,
-				// this.rotation.y * Math.cos(turnSpeed) - this.rotation.z * Math.sin(turnSpeed),
-				// this.rotation.y * Math.sin(turnSpeed) + this.rotation.z * Math.cos(turnSpeed)
-			// );
+			this.turn(X3D);
 		}	
 			
 	}
@@ -126,19 +115,7 @@ class Box {
 				.rotateAround(ZERO2D, turnSpeed);
 			this.index = new THREE.Vector3(temp.x, this.index.y, temp.y);
 			
-			// this.rotation = this.turn(new THREE.Vector3(0, -1, 0));
-			
-			// this.normal = new THREE.Vector3(
-				// this.normal.x * Math.cos(turnSpeed) - this.normal.z * Math.sin(turnSpeed),
-				// this.normal.y,
-				// this.normal.x * Math.sin(turnSpeed) + this.normal.z * Math.cos(turnSpeed)
-			// );
-
-			// this.rotation = new THREE.Vector3(
-				// this.rotation.x * Math.cos(turnSpeed) - this.rotation.z * Math.sin(turnSpeed),
-				// this.rotation.y,
-				// this.rotation.x * Math.sin(turnSpeed) + this.rotation.z * Math.cos(turnSpeed)
-			// );
+			this.turn(Y3D);
 		}
 	}
 	
@@ -155,217 +132,22 @@ class Box {
 				.rotateAround(ZERO2D, turnSpeed);
 			this.index = new THREE.Vector3(temp.x, temp.y, this.index.z);
 			
-			// this.rotation = this.turn(new THREE.Vector3(0, 0, 1));
-			
-			// this.normal = new THREE.Vector3(
-				// this.normal.x * Math.cos(turnSpeed) - this.normal.y * Math.sin(turnSpeed),
-				// this.normal.x * Math.sin(turnSpeed) + this.normal.y * Math.cos(turnSpeed),
-				// this.normal.z
-			// );
-			
-			// this.rotation = new THREE.Vector3(
-				// this.rotation.x * Math.cos(turnSpeed) - this.rotation.y * Math.sin(turnSpeed),
-				// this.rotation.x * Math.sin(turnSpeed) + this.rotation.y * Math.cos(turnSpeed),
-				// this.rotation.z
-			// );
+			this.turn(Z3D);
 		}
 	}
 	
 	turn(axis) {
-		return axis.multiplyScalar(turnSpeed).add(this.rotation);
+		this.group.rotateOnWorldAxis(axis, turnSpeed);
 	}
 	
-	rotateAround(vect, axis, angle) {
-		// Make sure our axis is a unit vector
-		axis = p5.Vector.normalize(axis);
-
-		return p5.Vector.add(
-			p5.Vector.mult(vect, Math.cos(angle)),
-			p5.Vector.add(
-				p5.Vector.mult(
-					p5.Vector.cross(axis, vect),
-					Math.sin(angle)
-				),
-				p5.Vector.mult(
-					p5.Vector.mult(
-						axis,
-						p5.Vector.dot(axis, vect)
-					),
-					(1 - Math.cos(angle))
-				)
-			)
-		);
-	}
-
 	update() {
 		this.group.position.round();
 		this.pos = this.group.position;
 		this.index.round();
 		this.oldIndex = this.index;
-		// switch (turn.direction) {
-			// case 'x':
-				// if (this.oldIndex.x == turn.index)
-					// this.rotation = new THREE.Vector3(this.rotation.x, -this.rotation.z, this.rotation.y);
-				// break;
-			// case 'y':
-				// if (this.oldIndex.y == turn.index)
-					// this.rotation = new THREE.Vector3(-this.rotation.z, this.rotation.y, this.rotation.x);
-				// break;
-			// case 'z':
-				// if (this.oldIandex.z == turn.index)
-					// this.rotation = new THREE.Vector3(-this.rotation.y, this.rotation.x, this.rotation.z);
-				// break;
-		// }
+		this.rotation = new THREE.Vector3(this.group.rotation.x, this.group.rotation.y, this.group.rotation.z);
 	}
 	
-	show() {
-		stroke(0);
-		strokeWeight(2);
-		// noStroke();
-		push();
-		
-		translate(this.pos);
-
-		// rotateX(this.rotation.x);
-		// let newRotation = new THREE.Vector3(
-			// this.rotation.x,
-			// this.rotation.y * Math.cos(-turnSpeed) - this.rotation.z * Math.sin(-turnSpeed),
-			// this.rotation.y * Math.sin(-turnSpeed) + this.rotation.z * Math.cos(-turnSpeed)
-		// );
-		// rotateZ(-this.rotation.y);
-		// rotateY(newRotation.y);
-
-		// applyMatrix(this.addAll3(this.rotation));
-		// rotateX(this.rotation.x);
-		// rotateY(this.rotation.y);
-		// rotateZ(this.rotation.z);
-		// if (this.rotation.mag() != 0)
-			rotateY(new THREE.Vector3(this.normal.x, 0, this.normal.z).angleBetween(this.start));
-
-
-		let r = this.len / 2;
-		fill(colors[0]);
-		beginShape();
-		vertex(-r, -r, r);
-		vertex(r, -r, r);
-		vertex(r, r, r);
-		vertex(-r, r, r);
-		endShape(CLOSE);
-
-		fill(colors[1]);
-		beginShape();
-		vertex(-r, -r, -r);
-		vertex(r, -r, -r);
-		vertex(r, r, -r);
-		vertex(-r, r, -r);
-		endShape(CLOSE);
-		
-		fill(colors[2]);
-		beginShape();
-		vertex(r, -r, -r);
-		vertex(r, -r, r);
-		vertex(r, r, r);
-		vertex(r, r, -r);
-		endShape(CLOSE);
-
-		fill(colors[3]);
-		beginShape();
-		vertex(-r, -r, -r);
-		vertex(-r, -r, r);
-		vertex(-r, r, r);
-		vertex(-r, r, -r);
-		endShape(CLOSE);
-		
-		fill(colors[4]);
-		beginShape();
-		vertex(-r, -r, -r);
-		vertex(-r, -r, r);
-		vertex(r, -r, r);
-		vertex(r, -r, -r);
-		endShape(CLOSE);
-
-		fill(colors[5]);
-		beginShape();
-		vertex(-r, r, -r);
-		vertex(-r, r, r);
-		vertex(r, r, r);
-		vertex(r, r, -r);
-		endShape(CLOSE);
-		
-		pop();
-		
-		stroke(16);
-		line(
-			this.pos.x, this.pos.y, this.pos.z, 
-			this.pos.x + this.normal.x * this.len,
-			this.pos.y + this.normal.y * this.len,
-			this.pos.z + this.normal.z * this.len
-		);
-	}
-	
-	highlight() {
-		stroke(0);
-		strokeWeight(2);
-		// noStroke();
-		push();
-		
-		translate(this.pos);
-		rotateX(this.rotation.x);
-		rotateY(this.rotation.y);
-		rotateZ(this.rotation.z);
-		// box(this.len);
-		let r = this.len / 2;
-		fill(colors[1]);
-		beginShape();
-		vertex(-r, -r, r);
-		vertex(r, -r, r);
-		vertex(r, r, r);
-		vertex(-r, r, r);
-		endShape(CLOSE);
-
-		fill(colors[1]);
-		beginShape();
-		vertex(-r, -r, -r);
-		vertex(r, -r, -r);
-		vertex(r, r, -r);
-		vertex(-r, r, -r);
-		endShape(CLOSE);
-		
-		fill(colors[1]);
-		beginShape();
-		vertex(r, -r, -r);
-		vertex(r, -r, r);
-		vertex(r, r, r);
-		vertex(r, r, -r);
-		endShape(CLOSE);
-
-		fill(colors[1]);
-		beginShape();
-		vertex(-r, -r, -r);
-		vertex(-r, -r, r);
-		vertex(-r, r, r);
-		vertex(-r, r, -r);
-		endShape(CLOSE);
-		
-		fill(colors[1]);
-		beginShape();
-		vertex(-r, -r, -r);
-		vertex(-r, -r, r);
-		vertex(r, -r, r);
-		vertex(r, -r, -r);
-		endShape(CLOSE);
-
-		fill(colors[1]);
-		beginShape();
-		vertex(-r, r, -r);
-		vertex(-r, r, r);
-		vertex(r, r, r);
-		vertex(r, r, -r);
-		endShape(CLOSE);
-		
-		pop();
-	}
-
 	addToScene(scene) {
 		scene.add(this.group);
 	}
